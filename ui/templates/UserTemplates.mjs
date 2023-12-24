6714a23c77ee2ac3cb13cee1ca159cfd696266ad
alexa <alex@targoninc.com>
2023-12-24 20:20:57 +0100

    fix login

--- ui/templates/UserTemplates.mjs
+++ ui/templates/UserTemplates.mjs
@@ -4,49 +4,53 @@ import {Auth} from "../js/Auth.mjs";
 export class UserTemplates {
     static login(router) {
         const form = FJS.create("div")
-            .classes("flex-v", "padded", "rounded", "centered", "align-content")
+            .classes("full-center")
             .children(
                 FJS.create("div")
-                    .classes("flex", "align-content")
+                    .classes("flex-v", "padded", "rounded", "centered", "align-content")
                     .children(
-                        FJS.create("label")
-                            .attributes("for", "username")
-                            .text("Username")
-                            .build(),
-                        FJS.create("input")
-                            .id("username")
-                            .name("username")
-                            .autocomplete("username")
-                            .type("text")
+                        FJS.create("div")
+                            .classes("flex", "align-content")
+                            .children(
+                                FJS.create("label")
+                                    .attributes("for", "username")
+                                    .text("Username")
+                                    .build(),
+                                FJS.create("input")
+                                    .id("username")
+                                    .name("username")
+                                    .autocomplete("username")
+                                    .type("text")
+                                    .build()
+                            ).build(),
+                        FJS.create("div")
+                            .classes("flex", "align-content")
+                            .children(
+                                FJS.create("label")
+                                    .attributes("for", "password")
+                                    .text("Password")
+                                    .build(),
+                                FJS.create("input")
+                                    .id("password")
+                                    .name("password")
+                                    .autocomplete("current-password")
+                                    .type("password")
+                                    .build()
+                            ).build(),
+                        FJS.create("div")
+                            .classes("flex", "align-content")
+                            .children(
+                                FJS.create("span")
+                                    .classes("login-error")
+                                    .build(),
+                            ).build(),
+                        FJS.create("button")
+                            .text("Submit")
+                            .onclick(async () => {
+                                await Auth.authorizeFromForm(router);
+                            })
                             .build()
-                    ).build(),
-                FJS.create("div")
-                    .classes("flex", "align-content")
-                    .children(
-                        FJS.create("label")
-                            .attributes("for", "password")
-                            .text("Password")
-                            .build(),
-                        FJS.create("input")
-                            .id("password")
-                            .name("password")
-                            .autocomplete("current-password")
-                            .type("password")
-                            .build()
-                    ).build(),
-                FJS.create("div")
-                    .classes("flex", "align-content")
-                    .children(
-                        FJS.create("span")
-                            .classes("login-error")
-                            .build(),
-                    ).build(),
-                FJS.create("button")
-                    .text("Submit")
-                    .onclick(async () => {
-                        await Auth.authorizeFromForm(router);
-                    })
-                    .build()
+                    ).build()
             ).build();
 
         form.addEventListener("keydown", async (e) => {
