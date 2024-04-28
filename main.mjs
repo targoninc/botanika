2de7fe8960a46be8f0f4584bf64cd1a83eaa9f2f
alexa <alex@targoninc.com>
2024-04-28 12:00:53 +0200

    add first ollama support

--- main.mjs
+++ main.mjs
@@ -3,12 +3,15 @@ import path from "path";
 import dotenv from "dotenv";
 import {Features} from "./lib/Features.mjs";
 import {activeEndpoints} from "./lib/endpoints/ActiveEndpoints.mjs";
+import {Completion} from "./lib/tooling/Completion.mjs";
 
 dotenv.config();
 const contextMap = {};
 
 const app = Features.addExpress();
 const db = await Features.addDatabase();
+
+await Completion.prepareProvider();
 
 Features.addEndpoints(app, contextMap, db, activeEndpoints);
 Features.addAuthentication(app, contextMap, db);
